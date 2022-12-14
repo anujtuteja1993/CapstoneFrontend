5273ab50ccd32aaae36a684bc7c3c9446dfad7ef
Anuj Tuteja <anujtuteja1993@gmail.com>
2022-12-15 00:16:18 +1300

    Made some minor changes

--- src/pages/CartDetails.js
+++ src/pages/CartDetails.js
@@ -9,38 +9,37 @@ const CartDetails = () => {
 
   useEffect(() => {
     const uniqueGames = [...new Set(gamesInCart)];
-    console.log(uniqueGames);
-    console.log(gamesInCart);
-      // console.log("Fetch" + i);
+    const gameMap= new Map();
+
+    gamesInCart.forEach(element => {
+      if(gameMap.has(element)){
+        gameMap.set(element, gameMap.get(element)+1);
+      }else{
+        gameMap.set(element, 1);
+      }
+    });
+
       fetch("http://localhost:8000/games/fetchGameByID?game_ids=[" + uniqueGames +"]")
         .then(resp => resp.json())
         .then(data => {
-          console.log(data.data);
-          setGamesInCartInfo(data.data);
+          let tempArr = [];
+          data.data.forEach((item, index) => {            
+            item.quantity = gameMap.get(`${item.id}`);
+            tempArr.push(item);
+          })
+          setGamesInCartInfo(tempArr);
         })
-        
 
-    // uniqueGames.forEach((game_id, index) => {
-    //   console.log("Fetch" + index);
-    //   fetch("http://localhost:8000/games/fetchGameByID?game_id=" + game_id)
-    //     .then(resp => resp.json())
-    //     .then(data => { console.log(data.data[0]);
-    //       setGamesInCartInfo(prevState => [...prevState, data.data[0]]);
-    //       // setGamesInCartInfo(data.data?.map(response => response.data.data))
-    //     })
-    // })
-  }, [gamesInCart]);  
-
-  // console.log(gamesInCartInfo);
+  }, [gamesInCart]);
 
   return (
     <div>
-      {/* {!gamesInCart.length && (
+      {!gamesInCart.length && (
         <div>no products in your shopping cart</div>
-      )} */}
+      )}
       {gamesInCartInfo?.map(gameInfo => {
         // const amount = gamesInCart.filter(function checkID(id){ return id === gameInfo.id})
-        console.log(gameInfo?.id);
+        // console.log(gameInfo?.id);
         return (
           <div className="flex mb-5 items-center" key={gameInfo.id}>
             <div className="bg-gray-100 p-3 rounded-xl shrink-0" style={{ boxShadow: 'inset 1px 0px 10px 10px rgba(0,0,0,0.1)' }}>
@@ -48,14 +47,13 @@ const CartDetails = () => {
             </div>
             <div className="pl-4 items-center">
               <h3 className="font-bold text-lg">{gameInfo.game_name}</h3>
-              {/* <p className="text-sm leading-4 text-gray-500">{productInfo.description}</p> */}
               <div className="flex mt-1">
                 <div className="grow font-bold">59.99</div>
                 <div>
                   {/* <button onClick={() => lessOfThisProduct(productInfo._id)} className="border border-emerald-500 px-2 rounded-lg text-emerald-500">-</button>*/}
-                  {/* <span className="px-2">
-                {gamesInCart.filter(item => item === gameInfo.id).length}
-              </span> */}
+                  <span className="px-2">
+                {gameInfo.quantity}
+              </span>
                   {/* <button onClick={() => moreOfThisProduct(productInfo._id)} className="bg-emerald-500 px-2 rounded-lg text-white">+</button>  */}
                 </div>
               </div>
